682b40c48a811c872c13b2766a9cb9e1033f21aa
Sameer Hameed <samorsameer@gmail.com>
2019-11-13 12:40:35 +0000

    Reading wallet from cloud

--- src/main/java/com/banking/opb/repo/DatabaseConfig.java
+++ src/main/java/com/banking/opb/repo/DatabaseConfig.java
@@ -1,18 +1,23 @@
 package com.banking.opb.repo;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import oracle.jdbc.pool.OracleDataSource;
@@ -33,21 +38,25 @@ public class DatabaseConfig {
     public DataSource dataSource() {
         OracleDataSource dataSource = null;
         try {
-        	
-        	 PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-
-             // Ant-style path matching
-            Resource[] resources = resolver.getResources("\\wallet\\");
-            String seprator = System.getProperty("file.separator");
+        	 String seprator = System.getProperty("file.separator");
             
-            String oracle_net_wallet_location = resources[0].getURI().toString().replace(resources[0].getFilename(), "");
-            oracle_net_wallet_location = oracle_net_wallet_location.replace("file:/", "");
-            oracle_net_wallet_location =oracle_net_wallet_location.replace("wallet/", "wallet");
-            oracle_net_wallet_location =oracle_net_wallet_location.replace("!", "");
-            
-            if ("\\".equals(seprator)) {
-            	oracle_net_wallet_location = oracle_net_wallet_location.replace("/", seprator);
+            String filename = "https://drive.google.com/uc?id=1QKsEANKUOsy6xGrVaEuJ34FIXGshuR1Q&export=download";
+    		
+			File assetsInfo = new File("wallet.zip");
+			FileUtils.copyURLToFile(new URL(filename), assetsInfo);
+			
+			String zipFilePath = assetsInfo.getAbsolutePath();
+			String path = zipFilePath.replace("\\wallet.zip", "");
+			
+			if ("\\".equals(seprator)) {
+				path = path.replace("/", seprator);
+				zipFilePath = zipFilePath.replace("/", seprator);
             }
+			
+			//unzip(zipFilePath, path);
+			unzipFile(zipFilePath);
+			String oracle_net_wallet_location = path + seprator + "wallet";
+			System.out.println(path + seprator + "wallet");
             
             dataSource = new OracleDataSource();
             Properties props = new Properties();
@@ -62,6 +71,45 @@ public class DatabaseConfig {
         }
         return dataSource;
     }
+	
+	public void unzipFile(String zipName) {
+	       // String zipName = "data.zip";
+	    	
+	        try (FileInputStream fis = new FileInputStream(zipName);
+	             ZipInputStream zis =
+	                 new ZipInputStream(new BufferedInputStream(fis))) {
+
+	            ZipEntry entry;
+	            int i = 0;
+
+	            // Read each entry from the ZipInputStream until no
+	            // more entry found indicated by a null return value
+	            // of the getNextEntry() method.
+	            while ((entry = zis.getNextEntry()) != null) {
+	            	if (i==0) {
+	            		File f = new File(entry.getName());
+	            		f.getParentFile().mkdirs();
+	            	}
+	                System.out.println("Unzipping: " + entry.getName());
+
+	                int size;
+	                byte[] buffer = new byte[2048];
+
+	                try (FileOutputStream fos =
+	                         new FileOutputStream(entry.getName());
+	                     BufferedOutputStream bos =
+	                         new BufferedOutputStream(fos, buffer.length)) {
+
+	                    while ((size = zis.read(buffer, 0, buffer.length)) != -1) {
+	                        bos.write(buffer, 0, size);
+	                    }
+	                    bos.flush();
+	                }
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
 
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
--- src/test/java/com/banking/opb/OpbApplicationTests.java
+++ src/test/java/com/banking/opb/OpbApplicationTests.java
@@ -1,13 +1,74 @@
 package com.banking.opb;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
-@SpringBootTest
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+
 class OpbApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws IOException {
+		String filename = "https://drive.google.com/uc?id=1QKsEANKUOsy6xGrVaEuJ34FIXGshuR1Q&export=download";
+
+		File assetsInfo = new File("wallet.zip");
+		FileUtils.copyURLToFile(new URL(filename), assetsInfo);
+
+		String zipFilePath = assetsInfo.getAbsolutePath();
+		String path = zipFilePath.replace("\\wallet.zip", "");
+
+		String seprator = System.getProperty("file.separator");
+
+		if ("\\".equals(seprator)) {
+			path = path.replace("/", seprator);
+			zipFilePath = zipFilePath.replace("/", seprator);
+		}
+
+		unzip(zipFilePath);
+		System.out.println(path + seprator + "wallet");
 	}
 
+	public void unzip(String zipName) {
+		// String zipName = "data.zip";
+
+		try (FileInputStream fis = new FileInputStream(zipName);
+				ZipInputStream zis = new ZipInputStream(new BufferedInputStream(fis))) {
+
+			ZipEntry entry;
+			int i = 0;
+
+			// Read each entry from the ZipInputStream until no
+			// more entry found indicated by a null return value
+			// of the getNextEntry() method.
+			while ((entry = zis.getNextEntry()) != null) {
+				if (i == 0) {
+					File f = new File(entry.getName());
+					f.getParentFile().mkdirs();
+				}
+				System.out.println("Unzipping: " + entry.getName());
+
+				int size;
+				byte[] buffer = new byte[2048];
+
+				try (FileOutputStream fos = new FileOutputStream(entry.getName());
+						BufferedOutputStream bos = new BufferedOutputStream(fos, buffer.length)) {
+
+					while ((size = zis.read(buffer, 0, buffer.length)) != -1) {
+						bos.write(buffer, 0, size);
+					}
+					bos.flush();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
